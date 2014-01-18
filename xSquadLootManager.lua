cbb1d5c0a3a4b641d337c5494b140ea61ad04602
Xsear <Xsear@users.noreply.github.com>
2014-01-18 23:25:32 +0100

    Version name change for dev purposes

--- xSquadLootManager.lua
+++ xSquadLootManager.lua
@@ -20,7 +20,7 @@ require './lib/lib_GTimer' -- Timer for rolltimeout
 require './lib/lib_LKObjects' -- Trivialize objects
 
 -- Constants
-csVersion = '0.90'
+csVersion = '0.91 Janstone'
 ciSaveVersion = 0.80
 
 -- Global Variables
