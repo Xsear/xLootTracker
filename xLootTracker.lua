38bc3d2eb41f079ee847c92de4b44d7bc4aeaf0d
Xsear <Xsear@users.noreply.github.com>
2014-08-16 01:49:05 +0200

    Set the addoninfo to dev

--- xLootTracker.lua
+++ xLootTracker.lua
@@ -30,8 +30,8 @@ require './lootpanel'
 
 -- Addon Meta
 AddonInfo = {
-    release  = "2014-08-14",
-    version = "1.12",
+    release  = "dev",
+    version = "1.13",
     patch = "1.0.1793",
     save = 1.0,
 }
