c43e4e7175bf2b4209e2b32cb123d55f595e17c2
Xsear <Xsear@users.noreply.github.com>
2014-09-05 22:29:20 +0200

    Cleaned up Messages.lua.

--- messages.lua
+++ messages.lua
@@ -1,30 +1,27 @@
--- Rename chat!
-
+--[[
+    Messages
+    Implements functions to send messages to chat, as well as a system for sending customizable messages in response to Tracker events.
+--]]
 Messages = {}
 
 local Private = {
-    ci_MessageLengthLimit = 255 -- Character limit of chat messages. Used to split too long messages into multiple. One character reserved for alerts.
+    messageLengthLimit = 255 -- Character limit of chat messages. Used to split too long messages into multiple. One character reserved for alerts.
 }
 
-
-
-
-
+--[[
+    Messages.OnTrackerNew(args)
+    Called when Tracker has added a new item.
+    Triggers the OnLootNew MessageEvent.
+--]]
 function Messages.OnTrackerNew(args)
     Messages.MessageEvent('Tracker', 'OnLootNew', args)
-
-    --[[
-    -- Messages
-    if Options['Messages']['Enabled'] then
-        local loot = Tracker.GetLootById(args.lootId)
-        local rarity = loot:GetRarity()
-        if rarity == LootRarity.Epic then
-            Chat.SendChannelText("squad", "Epic: "..tostring(loot:GetName()))
-        end
-    end
-    --]]
 end
 
+--[[
+    Messages.OnTrackerUpdate(args)
+    Called when Tracker has updated the status of an item.
+    Triggers the OnLootLooted and OnLootLost MessageEvents.
+--]]
 function Messages.OnTrackerUpdate(args)
     if args.previousState == LootState.Available then
         if args.newState == LootState.Looted then
@@ -35,111 +32,11 @@ function Messages.OnTrackerUpdate(args)
     end
 end
 
-
-
-
 --[[
-    Messages.SendChatMessage(channel, message, [alert])
-    For normal chat messages.
-]]--
-function Messages.SendChatMessage(channel, message, alert)
-    -- Requires that Core and Messages are Enabled
-    if not (Options['Core']['Enabled'] and Options['Messages']['Enabled']) then return end
-
-    -- Requires that you are the squad leader in order to send messages on squad channel
-    if channel == 'squad' and (not State.isSquadLeader and Options['Messages']['OnlyWhenSquadLeader']) then return end
-
-    -- Requires that you are the platoon leader in order to send messages on platoon channel
-    if channel == 'platoon' and (not State.isPlatoonLeader and Options['Messages']['OnlyWhenPlatoonLeader']) then return end
-
-    -- Handle optional arguments
-    alert = alert or false
-
-    -- Setup prefix
-    local prefix = Options['Messages']['Prefix']
-
-    -- Calculate message content length limit
-    local messageContentLengthLimit = Private.ci_MessageLengthLimit - unicode.len(Options['Messages']['Prefix'])
-
-    -- If the message is to long to send in one go, attempt to split lines into multiple messages
-    if unicode.len(message) > messageContentLengthLimit then
-        -- Explode the message on each new line
-        local messages = explode('\n', message)
-        local currentMessage = ''
-
-        -- For each line in the message
-        for num, line in ipairs(messages) do
-            --Debug.Log(tostring(num)..' : Length: '..unicode.len(message)..'/'..tostring(messageContentLengthLimit)..' : Line: '..line)
-            --Debug.Log('Message: '..currentMessage)
-
-            -- Warn if line is too long
-            if unicode.len(line) > messageContentLengthLimit then 
-                Debug.Warn('Unable to properly accommodate for message length, too many characters on line '..tostring(num))
-            end
-
-            -- On the very first iteration, just add message
-            if currentMessage == '' then
-                currentMessage = line
-
-            -- On subsequent iterations, we're gonna do some shit
-            else
-                -- If adding the next line exceeds the character limit
-                if unicode.len(currentMessage..'\n'..line) > messageContentLengthLimit then
-                    -- Send the current line and start a new message for the next line
-                    Messages.SendMessageToChat(channel, prefix..currentMessage, alert)
-                    currentMessage = line
-
-                -- Otherwise
-                else
-                    -- Add the next line to the current line
-                    currentMessage = currentMessage..'\n'..line
-                end
-            end
-        end
-        -- Make sure we send the last message
-        if currentMessage ~= '' then Messages.SendMessageToChat(channel, prefix..currentMessage, alert) end
-
-    -- Otherwise, send message normally
-    else
-        Messages.SendMessageToChat(channel, prefix..message, alert)
-    end
-end
-
-
-function Messages.SendFilteredMessage(channel, message, args)
-    message = Messages.TextFilters(message, args)
-    Messages.SendChatMessage(channel, message)
-end
-
-function Messages.SendSystemMessage(message)
-    local prefix = Lokii.GetString('SystemMessage_Prefix')
-    Messages.SendMessageToChat("system", prefix..message)
-end
-
--- Function to handle the actual sending of messages
-function Messages.SendMessageToChat(channel, message, alert)
-    channel = unicode.lower(channel)
-    if Options['Debug']['Enabled'] and Options['Debug']['SquadToArmy'] and channel == 'squad' then channel = 'army' end
-    local alertprefix = ''
-    if alert and (channel == "squad" or channel == "platoon") then alertprefix = '!' end
-    if channel == 'system' then
-        ChatLib.SystemMessage({text=message})
-    elseif channel == 'notification' or channel == 'notifications' then
-        ChatLib.Notification({text=message})
-    else
-        Chat.SendChannelText(channel, alertprefix..message)
-    end
-end
-
---[[
-    Messages.MessageEvent(eventClass, eventName, eventArgs, [canSend])
-    Generic function used to handle the process of sending messages in response to events, based on specific options.
-    Use the optional canSend argument to override bIsSquadLeader when determining whether or not to do anything.
+    Messages.MessageEvent(eventClass, eventName, eventArgs)
+    Handles the process of figuring out what message to send for an event, based on its options.
 --]]
-function Messages.MessageEvent(eventClass, eventName, eventArgs, canSend)
-    --canSend = canSend or State.isSquadLeader
-    --canSend and
-    --Debug.Table({event="MessageEvent", eventClass=eventClass, eventName=eventName, eventArgs=eventArgs, canSend=canSend})
+function Messages.MessageEvent(eventClass, eventName, eventArgs)
     if Options['Messages']['Events'][eventClass][eventName]['Enabled'] then
 
         if eventArgs.lootId then
@@ -189,21 +86,119 @@ function Messages.MessageEvent(eventClass, eventName, eventArgs, canSend)
                         end
                     end
                 end
-
             end
-
-
         end
-
     end
 end
 
+--[[
+    Messages.SendFilteredMessage(channel, message, args)
+    Allows for sending a formatted message outside of the event system.
+--]]
+function Messages.SendFilteredMessage(channel, message, args)
+    message = Messages.TextFilters(message, args)
+    Messages.SendChatMessage(channel, message)
+end
 
+--[[
+    Messages.SendChatMessage(channel, message, [alert])
+    Receives a full message to send to a channel, optionally as an alert.
+]]--
+function Messages.SendChatMessage(channel, message, alert)
+    -- Requires that Core and Messages are Enabled
+    if not (Options['Core']['Enabled'] and Options['Messages']['Enabled']) then return end
 
+    -- Requires that you are the squad leader in order to send messages on squad channel
+    if channel == 'squad' and (not State.isSquadLeader and Options['Messages']['OnlyWhenSquadLeader']) then return end
+
+    -- Requires that you are the platoon leader in order to send messages on platoon channel
+    if channel == 'platoon' and (not State.isPlatoonLeader and Options['Messages']['OnlyWhenPlatoonLeader']) then return end
+
+    -- Handle optional arguments
+    alert = alert or false
+
+    -- Setup prefix
+    local prefix = Options['Messages']['Prefix']
+
+    -- Calculate message content length limit
+    local messageContentLengthLimit = Private.messageLengthLimit - unicode.len(Options['Messages']['Prefix'])
+
+    -- If the message is to long to send in one go, attempt to split lines into multiple messages
+    if unicode.len(message) > messageContentLengthLimit then
+        -- Explode the message on each new line
+        local messages = explode('\n', message)
+        local currentMessage = ''
+
+        -- For each line in the message
+        for num, line in ipairs(messages) do
+            --Debug.Log(tostring(num)..' : Length: '..unicode.len(message)..'/'..tostring(messageContentLengthLimit)..' : Line: '..line)
+            --Debug.Log('Message: '..currentMessage)
+
+            -- Warn if line is too long
+            if unicode.len(line) > messageContentLengthLimit then 
+                Debug.Warn('Unable to properly accommodate for message length, too many characters on line '..tostring(num))
+            end
+
+            -- On the very first iteration, just add message
+            if currentMessage == '' then
+                currentMessage = line
+
+            -- On subsequent iterations, we're gonna do some shit
+            else
+                -- If adding the next line exceeds the character limit
+                if unicode.len(currentMessage..'\n'..line) > messageContentLengthLimit then
+                    -- Send the current line and start a new message for the next line
+                    Messages.SendMessageToChat(channel, prefix..currentMessage, alert)
+                    currentMessage = line
+
+                -- Otherwise
+                else
+                    -- Add the next line to the current line
+                    currentMessage = currentMessage..'\n'..line
+                end
+            end
+        end
+        -- Make sure we send the last message
+        if currentMessage ~= '' then Messages.SendMessageToChat(channel, prefix..currentMessage, alert) end
+
+    -- Otherwise, send message normally
+    else
+        Messages.SendMessageToChat(channel, prefix..message, alert)
+    end
+end
+
+--[[
+    Messages.SendSystemMessage(message)
+    Convenience function to send a system message.
+    Intended to be used by Slash commands.
+--]]
+function Messages.SendSystemMessage(message)
+    local prefix = Lokii.GetString('SystemMessage_Prefix')
+    Messages.SendMessageToChat("system", prefix..message)
+end
+
+--[[
+    Messages.SendMessageToChat(channel, message, alert)
+    Function to handle the actual sending of messages to chat.
+    Trusts blindly, does not check any options.
+--]]
+function Messages.SendMessageToChat(channel, message, alert)
+    channel = unicode.lower(channel)
+    if Options['Debug']['Enabled'] and Options['Debug']['SquadToArmy'] and channel == 'squad' then channel = 'army' end
+    local alertprefix = ''
+    if alert and (channel == "squad" or channel == "platoon") then alertprefix = '!' end
+    if channel == 'system' then
+        ChatLib.SystemMessage({text=message})
+    elseif channel == 'notification' or channel == 'notifications' then
+        ChatLib.Notification({text=message})
+    else
+        Chat.SendChannelText(channel, alertprefix..message)
+    end
+end
 
 --[[
     Messages.TextFilters(formatString, args)
-    Runs gsub filters for message formats.
+    The function that inserts information into a formatted string.
     Pretty poorly implemented at the moment.
 ]]--
 function Messages.TextFilters(formatString, args)
@@ -240,8 +235,6 @@ function Messages.TextFilters(formatString, args)
 
     return Private.replace_vars(formatString, replacementVars)
 end
-
-
 
 -- Source: http://lua-users.org/wiki/StringInterpolation
 -- Credit: http://lua-users.org/wiki/MarkEdgar
