f467a3ebd6462493a973af2e46c849d6e1dde556
Xsear <Xsear@users.noreply.github.com>
2014-09-05 22:51:57 +0200

    Cleaned up Sounds.lua. Fixed bug where its Private table was global.

--- sounds.lua
+++ sounds.lua
@@ -1,12 +1,23 @@
+--[[
+    Sounds
+    Plays sounds on tracker events.
+--]]
 Sounds = {}
-Private = {}
 
+local Private = {}
+
+--[[
+    Messages.OnTrackerNew(args)
+    Called when Tracker has added a new item.
+    Plays the New Loot sound as configured for the item.
+--]]
 function Sounds.OnTrackerNew(args)
     if Options['Sounds']['Enabled'] then
 
         local loot = Tracker.GetLootById(args.lootId)
 
         if not Options['Blacklist']['Sounds'][tostring(loot:GetTypeId())] and LootFiltering(loot, Options['Sounds']) then
+
             local categoryKey, rarityKey = GetLootFilteringOptionsKeys(loot, Options['Sounds']['Filtering'])
 
             System.PlaySound(Options['Sounds']['Filtering'][categoryKey][rarityKey]['SoundsNewLoot'])
